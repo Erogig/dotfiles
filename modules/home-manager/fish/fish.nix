782f6d71cd0aba32f9e05de3791be326c49728f6
Erogig <erogigabyte@gmail.com>
2026-01-19 23:26:34 +0100

    Try and fix fish

--- modules/home-manager/fish/fish.nix
+++ modules/home-manager/fish/fish.nix
@@ -6,5 +6,9 @@ programs.fish = {
     };
 
     home.shell.enableFishIntegration = true;
-    xdg.configFile."fish" = {source = ./../fish; force = true;};
+
+    xdg.configFile."fish/config.fish" = {source = ./../fish/config.fish; force = true;};
+    xdg.configFile."fish/fish_plugins" = {source = ./../fish/fish_plugins; force = true;};
+    xdg.configFile."fish/functions" = {source = ./../fish/functions; force = true;};
+    xdg.configFile."fish/themes" = {source = ./../fish/themes; force = true;};
 }
