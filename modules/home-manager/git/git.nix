1d88c23adb6f1b61396e3f321b6e9779a171fd18
Erogig <erogigabyte@gmail.com>
2026-01-19 22:45:34 +0100

    add git user details

--- modules/home-manager/git/git.nix
+++ modules/home-manager/git/git.nix
@@ -5,6 +5,8 @@ programs.git = {
     enable = true;
     settings = {
         init.defaultBranch = "main";
+        user.email = "erogigabyte@gmail.com";
+        user.name = "Erogig";
         };
     };
 }
