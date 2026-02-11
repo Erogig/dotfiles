d22f7774633030d8e74dd9ae77a05b99aa5539b3
Erogig <erogigabyte@gmail.com>
2026-02-11 22:45:03 +0100

    The great defaultification

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,14 +1,14 @@
 
 {
-    bash = import ./bash/bash.nix;
-    fish = import ./fish/fish.nix;
-    git = import ./git/git.nix;
-    hyprland = import ./hyprland/hyprland.nix;
-    rofi = import ./rofi/rofi.nix;
-    waybar = import ./waybar/waybar.nix;
-    hyprpaper = import ./hyprpaper/hyprpaper.nix;
-    kitty = import ./kitty/kitty.nix;
-    nvim = import ./nvim/nvim.nix;
-    vesktop = import ./vesktop/vesktop.nix;
-    dunst = import ./dunst/dunst.nix;
+    bash = import ./bash;
+    fish = import ./fish;
+    git = import ./git;
+    hyprland = import ./hyprland;
+    rofi = import ./rofi;
+    waybar = import ./waybar;
+    hyprpaper = import ./hyprpaper;
+    kitty = import ./kitty;
+    nvim = import ./nvim;
+    vesktop = import ./vesktop;
+    dunst = import ./dunst;
 }
