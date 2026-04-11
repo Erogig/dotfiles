d8b57022a5bd5b4ba83d7e29f70257254d4efae4
Erogig <erogigabyte@gmail.com>
2026-04-11 22:17:30 +0200

    waybar systemd

--- modules/home-manager/waybar/default.nix
+++ modules/home-manager/waybar/default.nix
@@ -3,6 +3,7 @@
 {
     programs.waybar = {
         enable = true;
+        systemd.enable = true;
         settings = builtins.fromJSON (builtins.readFile ./config.json);
         style = builtins.readFile ./style.css;
     };
